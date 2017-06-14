643d769144e7108e356ec11e673f70f97022c605
JValck <jasper_de_valck@hotmail.com>
2017-06-14 10:52:51 +0200

    Post installation facade

--- src/service/post_installation_facade.py
+++ src/service/post_installation_facade.py
@@ -20,6 +20,8 @@ Setarit - parcks[at]setarit.com
 """
 from __future__ import absolute_import
 
+from src.domain.parse.post_installation_parser import PostInstallationParser
+
 
 class PostInstallationFacade:
     def __init__(self, post_install_runnables):
@@ -29,6 +31,31 @@ class PostInstallationFacade:
         :type post_install_runnables: list of :class:`src.domain.model.post_install.post_install_runnable.PostInstallRunnable
         """
         self.post_install_runnables = post_install_runnables
+
+    @classmethod
+    def from_json(cls, json):
+        """
+        Create a PostInstallationFacade from a json object
+        :param json: The json
+        :return: The created PostInstallationFacade
+        """
+        json_array = cls._check_if_json_is_list(json)
+        parser = PostInstallationParser(json_array)
+        post_installation_runnables = parser.parse()
+        return cls(post_installation_runnables)
+
+    @classmethod
+    def _check_if_json_is_list(cls, json):
+        """
+        Checks if the json is a lost
+        :param json: The json to be verified
+        :return: The json as list
+        :rtype: list
+        """
+        if not isinstance(json, list):
+            return [json]
+        else:
+            return json
 
     def handle_post_installation(self):
         """
--- test/service/test_post_installation_facade.py
+++ test/service/test_post_installation_facade.py
@@ -20,9 +20,11 @@ Setarit - parcks[at]setarit.com
 """
 from __future__ import absolute_import
 
+import json
+
 from src.domain.log.logger import Logger
-from src.domain.model.post_install.post_install_runnable import PostInstallRunnable
-from src.domain.model.post_install.remote import Remote
+from src.domain.model.post_install.shell import Shell
+from src.domain.model.post_install.shell_command import ShellCommand
 
 from src.service.post_installation_facade import PostInstallationFacade
 import unittest
@@ -35,6 +37,8 @@ except ImportError:
 class TestPostInstallationFacade(unittest.TestCase):
     def setUp(self):
         self.create_facade()
+        self.create_json_object()
+        self.create_json_array()
         Logger.disable_all()
 
     def tearDown(self):
@@ -42,11 +46,67 @@ class TestPostInstallationFacade(unittest.TestCase):
 
     def create_facade(self):
         self.facade_under_test = PostInstallationFacade([
-            Remote("Dummy", "http://example.com"),
-            Remote("Dummy", "http://example.com")
+            Shell("Dummy", [ShellCommand(["ls"])]),
+            Shell("Dummy", [ShellCommand(["ls"])])
         ])
 
-    @patch.object(PostInstallRunnable, 'run')
+    def create_json_object(self):
+        Json = """\
+        {
+            "type":"shell",
+            "name":"Dummy",
+            "cmds":[
+                {
+                    "do":["ls"],
+                    "root":false
+                }
+            ]
+        }
+        """
+        self.json_object = json.loads(Json)
+
+    def create_json_array(self):
+        Json = """\
+        [
+            {
+                "type":"shell",
+                "name":"Dummy",
+                "cmds":[
+                    {
+                        "do":["ls"],
+                        "root":false
+                    }
+                ]
+            },
+            {
+                "type":"shell",
+                "name":"Dummy",
+                "cmds":[
+                    {
+                        "do":["ls"],
+                        "root":false
+                    }
+                ]
+            }
+        ]
+        """
+        self.json_list = json.loads(Json)
+
+    @patch.object(Shell, 'run')
     def test_handle_post_installation_calls_run_on_PostInstallRunnable(self, mock):
         self.facade_under_test.handle_post_installation()
         self.assertEqual(2, mock.call_count)
+
+    def test_from_json_creates_facade_with_list_if_json_list(self):
+        facade = PostInstallationFacade.from_json(self.json_list)
+        self.assertTrue(isinstance(facade.post_install_runnables, list))
+
+    def test_from_json_creates_facade_with_list_if_json_object(self):
+        facade = PostInstallationFacade.from_json(self.json_object)
+        self.assertTrue(isinstance(facade.post_install_runnables, list))
+
+    @patch.object(Shell, 'run')
+    def test_handle_post_installalation_calls_run_on_PostInstallRunnable_if_one_object(self, mock):
+        facade = PostInstallationFacade.from_json(self.json_object)
+        facade.handle_post_installation()
+        self.assertEqual(1, mock.call_count)
